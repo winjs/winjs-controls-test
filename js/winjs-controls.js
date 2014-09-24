014e2634b89e87ce03b4725561e9d792a1f73425
David Evans <david0evans@gmail.com>
2014-09-24 11:37:39 -0700

    Add split view in larger screens

--- js/winjs-controls.js
+++ js/winjs-controls.js
@@ -1,55 +1,75 @@
 window.addEventListener('load', function()
 {
-  var iframe = document.querySelector('#control-view');
-  var selectControl = document.querySelector('.version-select');
+  var iframes = document.querySelectorAll('.control-view');
+  var selectControlLeft = document.querySelector('.version-select.left');
+  var selectControlRight = document.querySelector('.version-select.right');
 
   //
   // Update the control display
   //
-  function update(path, version, theme)
+  function update(path, version, theme, index)
   {
-    iframe.src = path;
-    iframe.onload = function()
+    var iframesToUpdate = (typeof index === 'undefined') ? iframes : [iframes[index]];
+
+    for (var i = 0; i < iframesToUpdate.length; ++i)
     {
-      var existingStyle = iframe.contentDocument.head.querySelector('#winjs-stylesheet');
-      if (existingStyle)
-        iframe.contentDocument.head.removeChild(existingStyle);
+      var iframe = iframesToUpdate[i];
+      iframe.src = path;
 
-      // Add stylesheet
-      var style = document.createElement('link');
-      style.rel = 'stylesheet';
-      style.href = '../winjs/' + version + '/css/ui-' + theme + '.css';
-      style.id = 'winjs-stylesheet';
-      iframe.contentDocument.head.appendChild(style);
-
-      // Add javascript
-      var base = document.createElement('script');
-      var ui = document.createElement('script');
-      base.src = '../winjs/' + version + '/js/base.js';
-      ui.src = '../winjs/' + version + '/js/ui.js';
-      iframe.contentDocument.head.appendChild(base);
-      iframe.contentDocument.head.appendChild(ui);
-
-      // Run WinJS
-      ui.onload = function()
+      (function(iframe)
       {
-        iframe.contentWindow.WinJS.Navigation.history.backStack = [{}];
-        iframe.contentWindow.WinJS.UI.processAll();
-        if (iframe.contentWindow.initialize)
+        iframe.onload = function()
         {
-          iframe.contentWindow.initialize();
-        }
-      }
-    };
+          // Add stylesheet
+          var style = document.createElement('link');
+          style.rel = 'stylesheet';
+          style.href = '../winjs/' + version + '/css/ui-' + theme + '.css';
+          style.id = 'winjs-stylesheet';
+          iframe.contentDocument.head.appendChild(style);
+
+          // Add javascript
+          var base = document.createElement('script');
+          var ui = document.createElement('script');
+          base.src = '../winjs/' + version + '/js/base.js';
+          ui.src = '../winjs/' + version + '/js/ui.js';
+          base.async = false;
+          ui.async = false;
+          iframe.contentDocument.head.appendChild(base);
+          iframe.contentDocument.head.appendChild(ui);
+
+          // Run WinJS
+          ui.onload = function()
+          {
+            if (!iframe.contentWindow.WinJS)
+            {
+              console.warn('WinJS failed to load, skipping initialization', '(' + version + ')')
+              return;
+            }
+
+            iframe.contentWindow.WinJS.Navigation.history.backStack = [{}];
+            iframe.contentWindow.WinJS.UI.processAll();
+            if (iframe.contentWindow.initialize)
+            {
+              iframe.contentWindow.initialize();
+            }
+          }
+        };
+      })(iframe);
+    }
   }
 
   //
   // Listen for changes on the version dropdown
   //
-  selectControl.addEventListener('change', function()
+  selectControlLeft.addEventListener('change', function()
   {
-    var parts = selectControl.value.split(' ');
-    update(iframe.src, parts[0], parts[1]);
+    var parts = selectControlLeft.value.split(' ');
+    update(iframes[0].src, parts[0], parts[1], 0);
+  });
+  selectControlRight.addEventListener('change', function()
+  {
+    var parts = selectControlRight.value.split(' ');
+    update(iframes[1].src, parts[0], parts[1], 1);
   });
 
   //
@@ -61,15 +81,18 @@ window.addEventListener('load', function()
     var link = e.target.getAttribute('data-link');
     if (link)
     {
-      var parts = selectControl.value.split(' ');
-      update(link, parts[0], parts[1]);
+      var parts = selectControlLeft.value.split(' ');
+      update(link, parts[0], parts[1], 0);
+      parts = selectControlRight.value.split(' ');
+      update(link, parts[0], parts[1], 1);
     }
   });
 
   //
   // Default version and theme
   //
-  var parts = selectControl.value.split(' ');
-  update(iframe.src, parts[0], parts[1]);
+  var parts = selectControlLeft.value.split(' ');
+  var src = document.querySelector('.control-link').getAttribute('data-link');
+  update(src, parts[0], parts[1]);
 });
 
